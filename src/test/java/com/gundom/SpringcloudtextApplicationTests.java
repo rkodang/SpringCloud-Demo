696bfb308f34a63ece5d1e2451effee6a7e4f749
rkodang <245894317@qq.com>
2020-02-13 12:25:33 +0800

    Initial commit

---
+++ src/test/java/com/gundom/SpringcloudtextApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gundom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudtextApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
